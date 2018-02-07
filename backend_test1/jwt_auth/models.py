b3c2ceed70819e50a2f0e8bc9f5bfafa4f84bd63
Dominic Dorhat <aboldsduck@gmail.com>
2018-02-07 13:17:30 +0800

    Added CustomUserModel

--- backend_test1/jwt_auth/models.py
+++ backend_test1/jwt_auth/models.py
@@ -1,3 +1,10 @@
-from django.db import models
+# jwt_auth/models.py 
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import AbstractUser 
+
+class User(AbstractUser):
+    name = models.TextField(max_length = 200, blank = False)
+    student_id = models.PositiveIntegerField(max_length= 8, blank = False)
+    library_no = models.PositiveIntegerField(blank = False)
+    email = models.EmailField(unique = True)
