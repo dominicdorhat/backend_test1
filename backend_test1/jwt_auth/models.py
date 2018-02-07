9fadbe7ec4ac173aea1bef3c7b2ff19cd6df248f
Dominic Dorhat <aboldsduck@gmail.com>
2018-02-07 13:25:37 +0800

    Added migrations and altered CustomUserModel

--- backend_test1/jwt_auth/models.py
+++ backend_test1/jwt_auth/models.py
@@ -5,6 +5,6 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     name = models.TextField(max_length = 200, blank = False)
-    student_id = models.PositiveIntegerField(max_length= 8, blank = False)
+    student_id = models.PositiveIntegerField(blank = False)
     library_no = models.PositiveIntegerField(blank = False)
     email = models.EmailField(unique = True)
