945ceb0dbab418573fd17b578961217a26651a65
Dominic Dorhat <aboldsduck@gmail.com>
2018-02-07 18:25:03 +0800

    Added Refresh Token

--- backend_test1/jwt_auth/urls.py
+++ backend_test1/jwt_auth/urls.py
@@ -1,8 +1,9 @@
 # jwt_auth/urls.py 
 
 from django.urls import path 
-from rest_framework_jwt.views import obtain_jwt_token
+from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token
 
 urlpatterns = [
     path('api-token-auth/', obtain_jwt_token),
+    path('api-token-refresh/', refresh_jwt_token),
 ]
