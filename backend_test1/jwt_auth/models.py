ac1b35813038d9608ae701f15be1c15f86cd5999
Dominic Dorhat <aboldsduck@gmail.com>
2018-02-07 14:19:17 +0800

    Altered Custom User Model

--- backend_test1/jwt_auth/models.py
+++ backend_test1/jwt_auth/models.py
@@ -1,10 +1,16 @@
 # jwt_auth/models.py 
 
 from django.db import models
-from django.contrib.auth.models import AbstractUser 
+from django.contrib.auth.models import AbstractUser, UserManager 
+
+# class CustomUserManager(UserManager):
+#     pass
 
 class User(AbstractUser):
     name = models.TextField(max_length = 200, blank = False)
-    student_id = models.IntegerField(blank = False)
-    library_no = models.IntegerField(blank = False)
+    student_id = models.IntegerField(null = True)
+    library_no = models.IntegerField(null = True)
     email = models.EmailField(unique = True)
+
+    # Use UserManager to get the standard methods
+    objects = UserManager()
