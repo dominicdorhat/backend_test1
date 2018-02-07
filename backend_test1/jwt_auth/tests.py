16e4e800dd7b4f2b73fb8b8165a1537f68b0fee5
Choo Wei Ken <weikenchoo@gmail.com>
2018-02-07 22:20:50 +0800

    'added create user test'

--- backend_test1/jwt_auth/tests.py
+++ backend_test1/jwt_auth/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
-
+from .models import User
 # Create your tests here.
+class ModelTestCase(TestCase):
+
+    def setUp(self):
+        User.objects.create_user(username = 'domidog',name = 'domidog')
+
+    def test_model(self):
+        username = User.objects.get(id = 1)
+        expected_object_name = f'{username.name}'
+        self.assertEqual(expected_object_name,'domidog')
