c1fa009c47a42f5aebd01a0fb3830fa35b359eaf
Dominic Dorhat <aboldsduck@gmail.com>
2018-02-07 21:38:47 +0800

    Added token verification

--- backend_test1/backend_test1/settings.py
+++ backend_test1/backend_test1/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import datetime
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -133,3 +134,9 @@ REST_FRAMEWORK = {
 }
 
 AUTH_USER_MODEL = 'jwt_auth.User'
+
+JWT_AUTH = {
+    'JWT_EXPIRATION_DELTA': datetime.timedelta(hours = 1),
+    'JWT_ALLOW_REFRESH': True,
+    'JWT_VERIFY': True,
+}   
